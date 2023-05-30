9d6078d44a6f7c0247b691d7c13e75cf438c705c
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-30 12:57:03 +0400

    run every hour during the month of January

--- src/main/java/com/CRON/CRONApplication/Schedule.java
+++ src/main/java/com/CRON/CRONApplication/Schedule.java
@@ -5,11 +5,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    // 8. CRON expression for run every day at 3:00 AM
-    @Scheduled(cron = "0 0 3 * * *")
+    // 10. CRON expression for run every hour during the month of January
+    @Scheduled(cron = "0 0 * 1 * *")
     public void runJob() {
         System.out.println("Cron job is running...");
     }
+
+//    // 9. CRON expression for run every day at 3:00 AM
+//    @Scheduled(cron = "0 0 3 * * *")
+//    public void runJob() {
+//        System.out.println("Cron job is running...");
+//    }
 
 //    // 8. CRON expression for run every hour on weekdays between 9:00 AM and 5:00 PM
 //    @Scheduled(cron = "0 0 9-17 * * MON-FRI")
