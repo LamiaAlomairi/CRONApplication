c7d964fb47cced2366115335006c120bae175065
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-30 13:24:56 +0400

    run every hour but only on even-numbered days of the month

--- src/main/java/com/CRON/CRONApplication/Schedule.java
+++ src/main/java/com/CRON/CRONApplication/Schedule.java
@@ -5,11 +5,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    // 18. CRON expression for run at 11:30 PM every Friday the 13th
-    @Scheduled(cron = "0 30 23 13 * FRI")
+    // 19. CRON expression for run every hour but only on even-numbered days of the month
+    @Scheduled(cron = "0 0 * */2 * *")
     public void runJob() {
         System.out.println("Cron job is running...");
     }
+
+//    // 18. CRON expression for run at 11:30 PM every Friday the 13th
+//    @Scheduled(cron = "0 30 23 13 * FRI")
+//    public void runJob() {
+//        System.out.println("Cron job is running...");
+//    }
 
 //    // 17. CRON expression for run every 5 minutes on weekdays and every 15 minutes on weekends
 //    @Scheduled(cron = "0 */5 * * * SUN-THU && 0 */15 * * * FRI,SAT")
