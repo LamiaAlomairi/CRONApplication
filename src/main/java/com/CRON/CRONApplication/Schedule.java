8c139079d7f6605db5cf2790378a8d4d12e7e0ca
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-30 12:47:14 +0400

    CRON expression for run every Sunday at 7:30 PM and 10:30 PM

--- src/main/java/com/CRON/CRONApplication/Schedule.java
+++ src/main/java/com/CRON/CRONApplication/Schedule.java
@@ -5,11 +5,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    // 5. CRON expression for run every month on the first day at 3:00 AM
-    @Scheduled(cron = "0 0 3 1 * *")
+    // 6. CRON expression for run every Sunday at 7:30 PM and 10:30 PM
+    @Scheduled(cron = "0 30 19,22 * * SUN")
     public void runJob() {
         System.out.println("Cron job is running...");
     }
+
+//    // 5. CRON expression for run every month on the first day at 3:00 AM
+//    @Scheduled(cron = "0 0 3 1 * *")
+//    public void runJob() {
+//        System.out.println("Cron job is running...");
+//    }
 
 //    // 4. CRON expression for run every Monday at 8:00 AM
 //    @Scheduled(cron = "0 0 8 * * MON")
